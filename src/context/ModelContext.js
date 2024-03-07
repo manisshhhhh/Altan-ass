4093b7e7e7208e91932e325970d1bf194e8a5faf
Manish <2002mybaran@gmail.com>
2024-03-07 23:16:01 +0530

    Adding models functionality

--- src/context/ModelContext.js
+++ src/context/ModelContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const ModelContext = createContext();
 
@@ -8,6 +9,7 @@ export const ModelProvider = ({ children }) => {
     const [models, setModels] = useState([
         {
             id: 1,
+            username: "john",
             name: 'Image Recognition',
             image: '/images/image1.jpg',
             description: 'Description for Product 5',
@@ -15,6 +17,7 @@ export const ModelProvider = ({ children }) => {
         },
         {
             id: 2,
+            username: 'Manish',
             name: 'Natural Language Processing (NLP)',
             image: '/images/image2.jpg',
             description: 'Description for Product 3',
@@ -23,16 +26,31 @@ export const ModelProvider = ({ children }) => {
         },
         {
             id: 3,
+            username: 'David',
             name: 'Speech Recognition',
             image: '/images/image3.png',
             description: 'Description for Product 4',
             rating: 4.0,
 
         },
+        {
+            id: 4,
+            username: 'Jhonsan',
+            name: 'Speech Recognition',
+            image: 'https://plus.unsplash.com/premium_photo-1669324357471-e33e71e3f3d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww',
+            description: 'Description for Product 4',
+            rating: 4.0,
+        }
     ])
+
+    const addModel = (newModel) => {
+        newModel.id = uuidv4();
+        setModels([newModel, ...models]);
+    }
 
     return <ModelContext.Provider value={{
         models,
+        addModel,
     }}>
         {children}
     </ModelContext.Provider>
--- src/screens/AddModelScreen.jsx
+++ src/screens/AddModelScreen.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React ,{ useState, useContext } from 'react'
+import ModelContext from '../context/ModelContext'
 
 const AddModelScreen = () => {
+  const { addModel } = useContext(ModelContext);
+
+  const [model, setModel] = useState({
+    username: "",
+    name: "",
+    image: "",
+    description: "",
+    rating:"",
+  })
+
+  function handler(event) {
+        const { name, value } = event.target;
+        
+        setModel((prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    addModel(model);
+
+    setModel({
+      username: "",
+      name: "",
+      image: "",
+      description: "",
+      rating:"",
+    })
+  
+  };
+
   return (
-    <div>
-      <h1>Add Model</h1>
-    </div>
+    <form onSubmit={handleSubmit} className='form-container'>
+      <input
+          name='username'
+          placeholder='Enter the usename'
+          value={model.username}
+          onChange={handler}
+          required
+        />
+      
+        <input
+          name='name'
+          placeholder='Enter the model name'
+          value={model.name}
+          onChange={handler}
+          required
+        />
+      
+      <input
+         name='image'
+          placeholder='Enter the image url'
+          value={model.image}
+          onChange={handler}
+          required
+      />
+
+      <input
+          name='description'
+          placeholder='Enter the description'
+          value={model.description}
+          onChange={handler}
+          required
+      />
+
+      <input
+          name='rating'
+          placeholder='Enter the rating'
+          type='number'
+          value={model.rating}
+          onChange={handler}
+          required
+      />
+      
+      <button type="submit" className='btn'>Add Model</button>
+    </form>
   )
 }
 
